edad26296e36b8d1ff5f7fb55d19b0deef549710
Kennedi Nadeak <kennedinadeak235@gmail.com>
2023-09-11 19:28:27 +0700

    Create Movie Class

---
+++ Movies.Core/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿using Movies.Core.Entities.Base;
+
+namespace Movies.Core.Entities
+{
+    public class Movie : Entity
+    {
+        public string MovieName { get; set; } = string.Empty;
+        public string DirectorName { get; set; } = string.Empty;
+        public string ReleaseYear { get; set; } = string.Empty;
+
+    }
+}
