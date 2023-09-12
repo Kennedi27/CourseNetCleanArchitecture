4c47193ef6b51af0c624b08feb55b387126687a4
Kennedi Nadeak <kennedinadeak235@gmail.com>
2023-09-12 20:38:47 +0700

    Add base repository

--- Movies.Core/Repositories/Base/IRepository.cs
+++ Movies.Core/Repositories/Base/IRepository.cs
@@ -8,6 +8,6 @@ namespace Movies.Core.Repositories.Base
         Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
         Task UpdateAsync(T entity);
-        Task DeleteAsync(int id);
+        Task DeleteAsync(T entity);
     }
 }
--- Movies.Infrastructure/Data/MovieContextSeed.cs
+++ Movies.Infrastructure/Data/MovieContextSeed.cs
@@ -35,16 +35,16 @@ namespace Movies.Infrastructure.Data
         {
             return new List<Movie>()
             {
-                new Movie { MovieName = "KKN di Desa Penari", DirectorName = "Awi Suryadi", ReleaseYear="2022" },
-                new Movie { MovieName = "Warkop DKI Reborn: Jangkrik Boss! Part 1", DirectorName = "Anggy Umbara", ReleaseYear="2016" },
-                new Movie { MovieName = "Pengabdi Setan 2: Communion", DirectorName = "Joko Anwar", ReleaseYear="2022" },
-                new Movie { MovieName = "Dilan 1990", DirectorName = "Fajar Bustomi Pidi Baiq", ReleaseYear="2018" },
-                new Movie { MovieName = "Miracle in Cell No. 7", DirectorName = "Hanung Bramantyo", ReleaseYear="2022" },
-                new Movie { MovieName = "Dilan 1991", DirectorName = "Fajar Bustomi Pidi Baiq", ReleaseYear="2019" },
-                new Movie { MovieName = "Sewu Dino", DirectorName = "Kimo Stamboel", ReleaseYear="2023" },
-                new Movie { MovieName = "Laskar Pelangi", DirectorName = "Riri Riza", ReleaseYear="2008" },
-                new Movie { MovieName = "Habibie & Ainun", DirectorName = "Faozan Rizal", ReleaseYear="2012" },
-                new Movie { MovieName = "Pengabdi Setan", DirectorName = "Joko Anwar", ReleaseYear="2017" }
+                new Movie { MovieName = "KKN di Desa Penari", DirectorName = "Awi Suryadi", ReleaseYear = "2022" },
+                new Movie { MovieName = "Warkop DKI Reborn: Jangkrik Boss! Part 1", DirectorName = "Anggy Umbara", ReleaseYear = "2016" },
+                new Movie { MovieName = "Pengabdi Setan 2: Communion", DirectorName = "Joko Anwar", ReleaseYear = "2022" },
+                new Movie { MovieName = "Dilan 1990", DirectorName = "Fajar Bustomi Pidi Baiq", ReleaseYear = "2018" },
+                new Movie { MovieName = "Miracle in Cell No. 7", DirectorName = "Hanung Bramantyo", ReleaseYear = "2022" },
+                new Movie { MovieName = "Dilan 1991", DirectorName = "Fajar Bustomi Pidi Baiq", ReleaseYear = "2019" },
+                new Movie { MovieName = "Sewu Dino", DirectorName = "Kimo Stamboel", ReleaseYear = "2023" },
+                new Movie { MovieName = "Laskar Pelangi", DirectorName = "Riri Riza", ReleaseYear = "2008" },
+                new Movie { MovieName = "Habibie & Ainun", DirectorName = "Faozan Rizal", ReleaseYear = "2012" },
+                new Movie { MovieName = "Pengabdi Setan", DirectorName = "Joko Anwar", ReleaseYear = "2017" }
             };
         }
     }
