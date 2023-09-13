62c950f2de5f0a0e34ce877b0c197708484ef0f4
Kennedi Nadeak <kennedinadeak235@gmail.com>
2023-09-13 19:03:07 +0700

    Create Movie Auto Mapper

---
+++ Movies.Application/Mappers/MovieMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace Movies.Application.Mappers
+{
+    public class MovieMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new(() =>
+        {
+            var config = new MapperConfiguration(cnf =>
+            {
+                cnf.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cnf.AddProfile<MovieMappingProfile>();
+            });
+
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
