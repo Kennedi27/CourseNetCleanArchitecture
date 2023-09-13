7bc8d7ead3ded9ae1c5767f1d84f09a03c323df2
Kennedi Nadeak <kennedinadeak235@gmail.com>
2023-09-13 19:03:34 +0700

    Create Handler

---
+++ Movies.Application/Handlers/GetMoviesByDirectorNameHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Movies.Application.Mappers;
+using Movies.Application.Queries;
+using Movies.Application.Responses;
+using Movies.Core.Repositories;
+
+namespace Movies.Application.Handlers
+{
+    public class GetMoviesByDirectorNameHandler : IRequestHandler<GetMoviesByDirectorNameQuery, IEnumerable<MovieResponse>>
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public GetMoviesByDirectorNameHandler(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public async Task<IEnumerable<MovieResponse>> Handle(GetMoviesByDirectorNameQuery request, CancellationToken cancellationToken)
+        {
+            var listMovie = await _movieRepository.GetMovieByDirectorName(request.DirectorName);
+            var movieList = MovieMapper.Mapper.Map<IEnumerable<MovieResponse>>(listMovie);
+            return movieList;
+        }
+    }
+}
